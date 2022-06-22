95b181a33cd020316cac7aa8617194dbe0b40a4c
khawpea <tonkhaw.ka@hotmail.com>
2022-06-22 16:30:50 +0700

    finish 22june

---
+++ lib/bodys/non_finish_job.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:ungegat/widgets/show_text.dart';
+class NonFInishJob extends StatelessWidget {
+  const NonFInishJob({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ShowText(text: 'This is Non FinishJob',);
+  }
+}
