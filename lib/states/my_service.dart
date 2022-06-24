f0f45a4ec8a8e455e7ff49b2452341976a77f5a1
khawpea <tonkhaw.ka@hotmail.com>
2022-06-24 15:53:17 +0700

    Project success

--- lib/states/my_service.dart
+++ lib/states/my_service.dart
@@ -40,7 +40,7 @@ class _MyServiceState extends State<MyService> {
     var dataLogins = preferences.getStringList('data');
     print('dataLonigs ==> $dataLogins');
     widgets.add(NonFInishJob(dataUserLogins: dataLogins!));
-    widgets.add(FinishJob());
+    widgets.add(FinishJob(idOfficer: dataLogins[0],));
     setState(() {});
   }
 
